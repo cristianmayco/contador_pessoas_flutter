16ed239b43933275f3900d82d9fea07f25b84854
Cristian Mayco da Costa <cristianmayco@gmail.com>
2020-08-18 14:51:31 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Contador de Pessoas",
+    home: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          "Pessoas : 0",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              child: Text(
+                "+1",
+                style: TextStyle(fontSize: 24.0, color: Colors.white),
+              ),
+              onPressed: () {},
+            ),
+            FlatButton(
+              child: Text(
+                "-1",
+                style: TextStyle(fontSize: 24.0, color: Colors.white),
+              ),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        Text(
+          "Podem entrar!!",
+          style: TextStyle(
+              color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30.0),
+        )
+      ],
+    ),
+  ));
+}
